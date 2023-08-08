04c43e7b367d98451c545cfcfba17d51c13fae45
namakuandrew <thesky11.lay@gmail.com>
2023-08-08 12:33:15 +0700

    first init

---
+++ script.js
@@ -0,0 +1,5 @@
+//initialize variable
+const taskInput = document.querySelector(".task-input"),
+  filters = document.querySelectorAll(".filters span"),
+  clearAll = document.querySelector(".clear-btn"),
+  taskBox = document.querySelector(".task-box");
