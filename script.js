ba3006d89678aed79ca25c87f304f927d5c36afe
namakuandrew <thesky11.lay@gmail.com>
2023-08-11 15:12:35 +0700

    add javascript

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ const taskInput = document.querySelector(".task-input"),
   filters = document.querySelectorAll(".filters span"),
   clearAll = document.querySelector(".clear-btn"),
   taskBox = document.querySelector(".task-box");
+
+let editId,
+  isEditTask = false;
+todos = JSON.parse(localStorage.getItem("todo-list"));
+
+filters.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector("span.active").classList.remove("active");
+    btn.classList.add("active");
+    showtodo(btn.id);
+  });
+});
+
+function showtodo(filter) {
+  let listTag = "";
+  if (todos) {
+    todos.forEach((todo, id) => {
+      let completed = todo.status == "completed" ? "checked" : "";
+      if (filter == todo.status || filter == "all") {
+        listTag += "";
+      }
+    });
+  }
+}
