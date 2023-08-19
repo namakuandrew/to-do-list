73e53e65f500bc898af9a82abc1de8604e26909b
namakuandrew <thesky11.lay@gmail.com>
2023-08-19 12:19:52 +0700

    adding javascript and css

--- script.js
+++ script.js
@@ -21,9 +21,54 @@ function showtodo(filter) {
   if (todos) {
     todos.forEach((todo, id) => {
       let completed = todo.status == "completed" ? "checked" : "";
+      // logical OR operator (||) in in javascript
       if (filter == todo.status || filter == "all") {
-        listTag += "";
+        listTag += `<li class="task">
+        <label for="${id}">
+          <input
+            type="checkbox"
+            onclick="updateStatus(this)"
+            id="${id}"
+            ${completed}
+          />
+          <p class="${completed}">${todo.name}</p>
+        </label>
+        <div class="settings">
+          <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+          <ul class="task-menu">
+            <li onclick='editTask(${id}, "${todo.name}")'>
+              <i class="uil uil-pen"></i> Edit
+            </li>
+            <li onclick="deleteTask(${id}, '${filter}')">
+              <i class="uil uil-trash"></i> Delete
+            </li>
+          </ul>
+        </div>
+      </li>`;
       }
     });
   }
+
+  // set the innerhtml of taskbox to either listtag or a message indicating no tasks
+  taskBox.innerHTML = listTag || `<span> You don't have any task here</span>`;
+
+  // check if there any tasks/lists present
+  let checkTask = taskBox.querySelectorAll(".task");
+  if (!checkTask.length) {
+    // if no tasks/lists, remove active class from clearAll buton
+    clearAll.classList.remove("active");
+  } else {
+    // if tasks/lists present, add active class to clearAll button
+    clearAll.classList.add("active");
+  }
+  // check if the height of the taskBox is more than or equal to 265px
+  if (taskBox.offsetheight >= 265) {
+    // if height is more, add overflow class to taskBox
+    taskBox.classList.add("overflow");
+  } else {
+    // if height is less, remove overflow class from taskBox
+  }
 }
+
+// to show all the lists
+showtodo("all");
